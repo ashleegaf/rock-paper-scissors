9dafee1960b05b34cf05479d023cd0c0d92881d5
ashleegaf <ashleegaf@gmail.com>
2022-11-07 23:11:20 -0500

    Add closure to track scores in successive games

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -9,7 +9,6 @@ function playRound(playerSelection, computerSelection) {
   if (computerSelection === "rock") {
     if (playerSelection === "rock") {
       div.textContent = "It's a tie. You both played rock.";
-      return "tie";
     } else if (playerSelection === "paper") {
       div.textContent = "You win! Paper beats rock.";
       return "win";
@@ -23,7 +22,6 @@ function playRound(playerSelection, computerSelection) {
       return "lose";
     } else if (playerSelection === "paper") {
       div.textContent = "It's a tie. You both played paper.";
-      return "tie";
     } else {
       div.textContent = "You win! Scissors beats paper.";
       return "win";
@@ -37,43 +35,54 @@ function playRound(playerSelection, computerSelection) {
       return "lose";
     } else {
       div.textContent = "It's a tie. You both played scissors.";
-      return "tie";
     }
   }
 }
 
 function getGameResults(playerScore, computerScore) {
   if (computerScore > playerScore) {
-    return `You lose! Computer beat you ${computerScore} to ${playerScore}.`;
+    div.textContent = `You lose! Computer beat you ${computerScore} to ${playerScore}.`;
   } else if (playerScore > computerScore) {
-    return `You win! You beat computer ${playerScore} to ${computerScore}.`;
-  } else {
+    div.textContent = `You win! You beat computer ${playerScore} to ${computerScore}.`;
+  } /* else {
     return `It's a tie! You both scored ${playerScore} points.`;
+  } */
+}
+
+function createRoundResults() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  return (result) => {
+    if (playerScore === 5 || computerScore === 5) {
+      getGameResults(playerScore, computerScore);
+    } else {
+      if (result === "win") {
+        playerScore += 1;
+        console.log('ps: ', playerScore);
+      } else if (result === "lose") {
+        computerScore += 1;
+        console.log('cs: ', computerScore);
+      }  
+    }
+    
+    const playerScoreNode = document.querySelector("#player-score");
+    const computerScoreNode = document.querySelector("#computer-score");
+    playerScoreNode.textContent = `Player Score: ${playerScore}`;
+    computerScoreNode.textContent = `Computer Score: ${computerScore}`;
   }
 }
 
 function playGame(e) {
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  // for (let i = 0; i < 5; i++) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = e.target.id;
-    const result = playRound(playerSelection, computerSelection);
-
-    if (result === "win") {
-      playerScore += 1;
-    } else if (result === "lose") {
-      computerScore += 1;
-    }
-  // }
-
-  return getGameResults(playerScore, computerScore);
+  const computerSelection = getComputerChoice();
+  const playerSelection = e.target.id;
+  const result = playRound(playerSelection, computerSelection);
+  getRoundResults(result);
 }
+
+const getRoundResults = createRoundResults();
 
 const div = document.querySelector("div");
 
-const computerSelection = getComputerChoice();
-
 const buttons = document.querySelectorAll("button");
-buttons.forEach(button => button.addEventListener('click', (e) => playRound(e.target.id, computerSelection)));
+buttons.forEach(button => button.addEventListener('click', playGame));
